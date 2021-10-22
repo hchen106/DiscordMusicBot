75caa0f6da721e1b176851008fae155a5c3b4b0b
Huihao Chen <hchen106@ucsc.edu>
2021-10-22 13:22:33 -0700

    speech function test

---
+++ speechrecognition.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+with mic as source:
+    print("talk now")
+    audio = r.listen(source)
+    print("Time Over")
+
+    try:
+        print("You said: " + r.recognize_google(audio, language="zh"))
+    except:
+        print("I didn't get that")
